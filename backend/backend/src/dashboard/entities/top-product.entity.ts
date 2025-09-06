76bc4abd5c273f7ac876918fcd689f3111ec35ab
ChethakaL <chethaka.sl@gmail.com>
2025-09-07 01:11:11 +0530

    add @types/swagger-ui-react for TypeScript support

--- backend/backend/src/dashboard/entities/top-product.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class TopProduct {
-    @PrimaryGeneratedColumn() id: number;
-    @Column() name: string;
-    @Column('int') pct: number;
-    @Column() color: string;
-}
